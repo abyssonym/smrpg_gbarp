2477aacd19146ba737d2fdb55694f357a967b977
Anonymous <none>
2016-08-11 00:24:20 -0700

    implement randomized stat curves for characters

--- randomizer.py
+++ randomizer.py
@@ -15,6 +15,10 @@ ALL_OBJECTS = None
 
 class CharIndexObject:
     @property
+    def level(self):
+        return (self.index / 5) + 2
+
+    @property
     def character_id(self):
         return self.index % 5
 
@@ -22,6 +26,43 @@ class CharIndexObject:
     def get_by_character(cls, character, index):
         candidates = [c for c in cls.every if c.character_id == character]
         return candidates[index]
+
+
+class StatObject(CharIndexObject):
+    @property
+    def attack(self):
+        return self.physical >> 4
+
+    @property
+    def defense(self):
+        return self.physical & 0xF
+
+    @property
+    def magic_attack(self):
+        return self.magical >> 4
+
+    @property
+    def magic_defense(self):
+        return self.magical & 0xF
+
+    def set_stat(self, attr, value):
+        assert 0 <= value <= 0xF
+        if attr == "max_hp":
+            self.max_hp = value
+            return
+
+        if attr in ["attack", "defense"]:
+            affected = "physical"
+        elif attr in ["magic_attack", "magic_defense"]:
+            affected = "magical"
+
+        oldvalue = getattr(self, affected)
+        if attr in ["attack", "magic_attack"]:
+            newvalue = (oldvalue & 0xF) | (value << 4)
+        elif attr in ["defense", "magic_defense"]:
+            newvalue = (oldvalue & 0xF0) | value
+
+        setattr(self, affected, newvalue)
 
 
 class MonsterObject(TableObject):
@@ -81,27 +122,170 @@ class MonsterAttackObject(TableObject): pass
 class MonsterRewardObject(TableObject): pass
 class PackObject(TableObject): pass
 class FormationObject(TableObject): pass
+
+
 class CharacterObject(TableObject):
+    flag = "c"
+    flag_description = "character stats"
+
+    mutate_attributes = {"speed": (1, 0xFF),
+                         "level": (1, 30),
+                         }
+
     def cleanup(self):
+        self.current_hp = self.max_hp
+
         my_learned = [l for l in LearnObject.every if l.level <= self.level
                       and l.character_id == self.index]
         for l in my_learned:
             if l.spell <= 0x1A:
                 self.known_spells |= (1 << l.spell)
 
+        my_growths = [s for s in StatGrowthObject.every if
+                      s.level <= self.level and s.character_id == self.index]
+        for g in my_growths:
+            for attr in ["max_hp", "attack", "defense",
+                         "magic_attack", "magic_defense"]:
+                setattr(self, attr, getattr(self, attr) + getattr(g, attr))
+
+        if self.level == 1:
+            self.xp = 0
+        else:
+            self.xp = LevelUpXPObject.get(self.level-2).xp
+
 
 class ItemObject(TableObject): pass
 class PriceObject(TableObject): pass
 class LevelUpXPObject(TableObject): pass
-class StatGrowthObject(CharIndexObject, TableObject): pass
-class StatBonusObject(CharIndexObject, TableObject): pass
+
+
+class StatGrowthObject(StatObject, TableObject):
+    flag = "c"
+
+    @classmethod
+    def full_randomize(cls):
+        if hasattr(cls, "after_order"):
+            for cls2 in cls.after_order:
+                if not (hasattr(cls2, "randomized") and cls2.randomized):
+                    raise Exception("Randomize order violated.")
+        attributes = ["max_hp", "attack", "defense",
+                      "magic_attack", "magic_defense"]
+        curves = defaultdict(list)
+        for character_index in range(5):
+            c = CharacterObject.get(character_index)
+            for attr in attributes:
+                value = getattr(c, attr)
+                for l in cls.every:
+                    if l.character_id == c.index and l.level <= 20:
+                        value += getattr(l, attr)
+                value = mutate_normal(value, maximum=999)
+                fixed_points = [(1, 0), (20, value)]
+                for _ in xrange(3):
+                    dex = random.randint(1, len(fixed_points)-1)
+                    lower_level, lower_value = fixed_points[dex-1]
+                    upper_level, upper_value = fixed_points[dex]
+                    if upper_level - lower_level < 4:
+                        continue
+                    level_interval = (upper_level - lower_level) / 2
+                    value_interval = (upper_value - lower_value) / 2
+                    level = (lower_level + random.randint(0, level_interval)
+                             + random.randint(0, level_interval))
+                    if level <= lower_level or level >= upper_level:
+                        continue
+                    value = (lower_value + random.randint(0, value_interval)
+                             + random.randint(0, value_interval))
+                    fixed_points.append((level, value))
+                    fixed_points = sorted(fixed_points)
+
+                for ((l1, v1), (l2, v2)) in zip(fixed_points, fixed_points[1:]):
+                    ldist = l2 - l1
+                    vdist = v2 - v1
+                    for l in range(l1+1, l2):
+                        factor = (l - l1) / float(ldist)
+                        v = v1 + (factor * vdist)
+                        fixed_points.append((l, int(round(v))))
+                fixed_points = sorted(fixed_points)
+                levels, values = zip(*fixed_points)
+                assert len(fixed_points) == 20
+                assert levels == tuple(sorted(levels))
+                assert values == tuple(sorted(values))
+                increases = []
+                for v1, v2 in zip(values, values[1:]):
+                    increases.append(v2-v1)
+
+                frontload_factor = random.random() * random.random()
+                if attr in ["defense", "magic_defense"]:
+                    frontload_factor *= random.random()
+                frontloaded = 0
+                for n, inc in enumerate(increases):
+                    max_index = len(increases) - 1
+                    factor = (((max_index-n) / float(max_index))
+                              * frontload_factor)
+                    amount = int(round(inc * factor))
+                    frontloaded += amount
+                    increases[n] = (inc - amount)
+                frontloaded = max(frontloaded, 1)
+
+                while max(increases) > 15:
+                    i = increases.index(max(increases))
+                    increases[i] = increases[i] - 1
+                    choices = [n for (n, v) in enumerate(increases) if v < 15]
+                    if random.randint(0, len(choices)) == 0:
+                        frontloaded += 1
+                    elif choices:
+                        i = random.choice(choices)
+                        increases[i] = increases[i] + 1
+
+                curves[attr].append((frontloaded, increases))
+
+        for attr in attributes:
+            attr_curves = curves[attr]
+            random.shuffle(attr_curves)
+            for character_index in xrange(5):
+                (base, increases) = attr_curves.pop()
+                c = CharacterObject.get(character_index)
+                if c.index == 0 and attr in ["max_hp", "attack"]:
+                    # ensure basic starting stats for Mario
+                    while base < 20:
+                        base += 1
+                        for i in range(len(increases)):
+                            if increases[i] > 0:
+                                increases[i] = increases[i] - 1
+                                break
+                getattr(c, attr)
+                setattr(c, attr, base)
+                assert len(increases) == 19
+                for s in StatGrowthObject.every:
+                    if s.character_id == c.index:
+                        if increases:
+                            s.set_stat(attr, increases.pop(0))
+                        else:
+                            s.set_stat(attr, mutate_normal(2))
+
+
+class StatBonusObject(StatObject, TableObject):
+    flag = "c"
+    intershuffle_attributes = ["max_hp", "physical", "magical"]
+
+    @property
+    def intershuffle_valid(self):
+        return self.level <= 20
+
+    def mutate(self):
+        valids = [s for s in StatBonusObject.every if s.intershuffle_valid and
+                  all([getattr(s, attr)
+                       for attr in self.intershuffle_attributes])]
+        for attr in self.intershuffle_attributes:
+            while getattr(self, attr) == 0:
+                setattr(self, attr, getattr(random.choice(valids), attr))
+
+
 class SpellObject(TableObject): pass
 
 
 class LearnObject(CharIndexObject, TableObject):
-    @property
-    def level(self):
-        return (self.index / 5) + 2
+    flag = "s"
+    flag_description = "character spells"
 
     @property
     def rank(self):
@@ -157,6 +341,6 @@ if __name__ == "__main__":
         rewrite_snes_meta("SMRPG-R", VERSION, megabits=32, lorom=True)
         finish_interface()
         import pdb; pdb.set_trace()
-    except Exception, e:
+    except ValueError, e:
         print "ERROR: %s" % e
         raw_input("Press Enter to close this program.")
