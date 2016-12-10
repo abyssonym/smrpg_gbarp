5d0c98dd6bb3430e6dcc778c8a2585951432b57f
Anonymous <none>
2016-12-10 07:17:37 -0800

    update version 4

--- randomizer.py
+++ randomizer.py
@@ -9,7 +9,7 @@ from collections import defaultdict
 from os import path
 
 
-VERSION = 3
+VERSION = 4
 ALL_OBJECTS = None
 LEVEL_STATS = ["max_hp", "attack", "defense", "magic_attack", "magic_defense"]
 EQUIP_STATS = ["speed", "attack", "defense", "magic_attack", "magic_defense"]
